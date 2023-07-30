bf95164b019e359115ea8f72375db051cb1460a3
Philippe Carphin <philippe.carphin@polymtl.ca>
2023-07-30 15:46:25 -0400

    Require two failed pings before declaring generate

--- pings.sh
+++ pings.sh
@@ -7,24 +7,26 @@
 # then the time between checks will be closer to
 # $((time_between_checks + ping_timeout))
 
-ping_timeout=7
+ping_timeout=2
 time_between_checks=25
 
 # Google IP to try if I suspect a DNS problem: 172.217.13.110
 
 set -o pipefail
 function internet-works(){
-    timeout ${ping_timeout} bash -c "ping -c 1 google.com | head -n 2 | tail -n 1 | awk '{print \$7}'"
+    timeout ${1} bash -c "ping -c 1 google.com | head -n 2 | tail -n 1 | awk '{print \$7}'"
 }
 
 function main(){
     local result
     while true ; do
 
-        if result=$(internet-works) ; then
+        if result=$(internet-works ${ping_timeout}) ; then
             echo "Internet working at $(date "+%Y-%m-%d %T") (${result})"
         else
-            echo "Internet down at $(date "+%Y-%m-%d %T")"
+            if ! result=$(internet-works 0.4) ; then
+                echo "Internet down at $(date "+%Y-%m-%d %T")"
+            fi
         fi
 
         sleep ${time_between_checks}
